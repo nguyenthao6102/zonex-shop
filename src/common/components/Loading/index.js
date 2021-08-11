c220c8be1b6654233a8a9dd8a21eb17a5f22d553
thaoxinhtrai77 <nguyenthao6102@gmail.com>
2021-08-11 23:16:43 +0700

    fix interface

--- src/common/components/Loading/index.js
+++ src/common/components/Loading/index.js
@@ -1,11 +1,11 @@
 import React from "react";
-import loadingImg from "../../../assets/images/loading.gif";
+import loadingImg from "../../../assets/images/loading-5.gif";
 import "./index.scss";
 
 function Loading() {
 	return (
 		<div className="loading">
-			<img src={loadingImg} alt="Loading" width="60px" height="60px" />
+			<img src={loadingImg} alt="Loading" />
 		</div>
 	);
 }
