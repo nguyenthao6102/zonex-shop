a0a2ae05447984676dfc14b129352f55debd7110
thaoxinhtrai77 <nguyenthao6102@gmail.com>
2021-08-27 22:52:10 +0700

    using Lazy Suspense

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { lazy, Suspense } from "react";
 import { useSelector } from "react-redux";
 import {
 	BrowserRouter as Router,
@@ -8,18 +9,20 @@ import {
 import "./App.scss";
 import Footer from "./common/components/Footer";
 import Header from "./common/components/Header";
+import Loading from "./common/components/Loading";
 import ModalProduct from "./common/components/ModalProduct";
-import ScrollToTop from "./common/components/ScrollToTop";
 import ToastMessage from "./common/components/ToastMessage";
-import AboutPage from "./pages/AboutPage";
-import AccountPage from "./pages/AccountPage";
-import CartPage from "./pages/CartPage";
-import ContactPage from "./pages/ContactPage";
-import DetailProductPage from "./pages/DetailProductPage";
-import ErrorPage from "./pages/ErrorPage";
-import HomePage from "./pages/HomePage";
-import OrdersPage from "./pages/OrdersPage";
-import ProductsPage from "./pages/ProductsPage";
+
+const ScrollToTop = lazy(() => import("./common/components/ScrollToTop"));
+const AboutPage = lazy(() => import("./pages/AboutPage"));
+const AccountPage = lazy(() => import("./pages/AccountPage"));
+const CartPage = lazy(() => import("./pages/CartPage"));
+const ContactPage = lazy(() => import("./pages/ContactPage"));
+const DetailProductPage = lazy(() => import("./pages/DetailProductPage"));
+const ErrorPage = lazy(() => import("./pages/ErrorPage"));
+const HomePage = lazy(() => import("./pages/HomePage"));
+const OrdersPage = lazy(() => import("./pages/OrdersPage"));
+const ProductsPage = lazy(() => import("./pages/ProductsPage"));
 
 function App() {
 	const message = useSelector((state) => state.shop.showMessage);
@@ -29,60 +32,62 @@ function App() {
 
 	return (
 		<Router>
-			<div className="App">
-				<Header />
+			<Suspense fallback={<Loading />}>
+				<div className="App">
+					<Header />
 
-				<Switch>
-					<Route exact path="/">
-						<HomePage />
-					</Route>
+					<Switch>
+						<Route exact path="/">
+							<HomePage />
+						</Route>
 
-					<Route exact path="/products">
-						<ProductsPage />
-					</Route>
+						<Route exact path="/products">
+							<ProductsPage />
+						</Route>
 
-					<Route exact path="/products/:id">
-						<DetailProductPage />
-					</Route>
+						<Route exact path="/products/:id">
+							<DetailProductPage />
+						</Route>
 
-					<Route exact path="/about">
-						<AboutPage />
-					</Route>
+						<Route exact path="/about">
+							<AboutPage />
+						</Route>
 
-					<Route exact path="/contact">
-						<ContactPage />
-					</Route>
+						<Route exact path="/contact">
+							<ContactPage />
+						</Route>
 
-					<Route exact path="/cart">
-						<CartPage />
-					</Route>
+						<Route exact path="/cart">
+							<CartPage />
+						</Route>
 
-					<Route
-						exact
-						path="/account"
-						render={() => {
-							return !auth ? <AccountPage /> : <Redirect to="/" />;
-						}}
-					></Route>
+						<Route
+							exact
+							path="/account"
+							render={() => {
+								return !auth ? <AccountPage /> : <Redirect to="/" />;
+							}}
+						></Route>
 
-					<Route
-						exact
-						path="/orders"
-						render={() => {
-							return auth ? <OrdersPage /> : <Redirect to="/account" />;
-						}}
-					></Route>
+						<Route
+							exact
+							path="/orders"
+							render={() => {
+								return auth ? <OrdersPage /> : <Redirect to="/account" />;
+							}}
+						></Route>
 
-					<Route>
-						<ErrorPage />
-					</Route>
-				</Switch>
+						<Route>
+							<ErrorPage />
+						</Route>
+					</Switch>
 
-				{message && <ToastMessage />}
-				{modalProduct && <ModalProduct />}
-				<Footer />
-			</div>
-			<ScrollToTop />
+					{message && <ToastMessage />}
+					{modalProduct && <ModalProduct />}
+					<Footer />
+				</div>
+				<ScrollToTop />
+			</Suspense>
 		</Router>
 	);
 }
